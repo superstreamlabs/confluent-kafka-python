9f2a997abe8835d51ad415a712e7ab4d4f569118
Bazen <49089563+bazen-teklehaymanot@users.noreply.github.com>
2024-09-12 14:08:14 +0300

    Add SASL mechanisms into auth keys

--- src/confluent_kafka/superstream/utils.py
+++ src/confluent_kafka/superstream/utils.py
@@ -544,6 +544,7 @@ class KafkaUtil:
             "sasl.username",
             "sasl.password",
             "sasl.mechanism",
+            "sasl.mechanisms",
             "ssl.ca.location",
         ]
 
